dc520d6603790994f077af24506a80957f0212ed
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-22 19:46:55 +0100

    Add CoinType enum

---
+++ src/main/java/Coin/CoinType.java
@@ -0,0 +1,22 @@
+package Coin;
+
+public enum CoinType {
+
+    ONE(1),
+    TWO(2),
+    TEN(10),
+    TWENTY(20),
+    FIFTY(50),
+    ONEHUNDRED(100),
+    TWOHUNDRED(200);
+
+    private final int value;
+
+    CoinType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
