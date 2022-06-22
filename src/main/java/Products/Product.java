a57f9a9d9c7f62962f4ed3bacf5f8fed3ec57584
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-22 20:26:27 +0100

    Add abstract class Product

---
+++ src/main/java/Products/Product.java
@@ -0,0 +1,33 @@
+package Products;
+
+public abstract class Product {
+
+    private final String name;
+    private final int price;
+    private final String code;
+    private final int stock;
+
+
+    public Product(String name, int price, String code, int stock) {
+        this.name = name;
+        this.price = price;
+        this.code = code;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
